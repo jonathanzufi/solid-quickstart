64f26fdb0f13ef9d9255fd660b4201837373d34d
Jonathan Zufi <jonathanzufi@gmail.com>
2024-05-21 14:56:38 -0400

    Update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,11 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>ICONIC - A Photographic Tribute to Apple Innovation</Title>
+      <h1>ICONIC - A Photographic Tribute to Apple Innovation</h1>
       <Counter />
       <p>
+        <img src="../img/iconicmain.png"></img>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
