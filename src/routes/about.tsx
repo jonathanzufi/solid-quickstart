7466739d38b0213505e6aa273abff54330e00c4b
Jonathan Zufi <jonathanzufi@gmail.com>
2024-05-27 14:55:41 -0400

    Updates

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -7,7 +7,22 @@ export default function Home() {
       <h1>ICONIC - A Photographic Tribute to Apple Innovation</h1>
 
       <p>
+        <img src="../img/desktops.webp"></img>
+      </p>
+      <p>
         <img src="../img/ad-portables.webp"></img>
+      </p>
+      <p>
+        <img src="../img/idevices.webp"></img>
+      </p>
+      <p>
+        <img src="../img/packaging.webp"></img>
+      </p>
+      <p>
+        <img src="../img/peripherals.webp"></img>
+      </p>
+      <p>
+        <img src="../img/prototypes.webp" width="100%"></img>
       </p>
       <p>About</p>
     </main>
